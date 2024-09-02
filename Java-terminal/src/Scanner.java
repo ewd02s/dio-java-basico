804be7c2da922a4f011b94353689594e57b66b00
Ewd02s <ewd02s@gmail.com>
2024-09-02 19:17:05 -0300

    Terminal e Argumentos - Aprendendo a Sintaxe Java

---
+++ Java-terminal/src/Scanner.java
@@ -0,0 +1,24 @@
+import java.io.InputStream;
+
+public class Scanner {
+
+    public Scanner(InputStream in) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public String next() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'next'");
+    }
+
+    public int nextInt() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'nextInt'");
+    }
+
+    public double nextDouble() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'nextDouble'");
+    }
+
+}
